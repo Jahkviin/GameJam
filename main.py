8c250bd228f4ccb45250fcca87fba36221f1ed5d
Oliver Björnham <olibjo@elevad.umea.se>
2024-11-01 22:13:59 +0100

    Pygame core code

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-import Pygame
+import pygame
 
-print("hello world")
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Logic here
+
+    screen.fill("purple")
+
+    # RENDER GAME HERE
+
+    pygame.display.flip()
+
+    clock.tick(20)  # limits FPS to 60
+
+pygame.quit()
