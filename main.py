871cf5798ca75114d381ba0a9f9f0b0820747ba5
Oliver Björnham <olibjo@elevad.umea.se>
2024-11-02 14:56:43 +0100

    Added VHS visuals and death logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import player
 import object
+import os
+import math
 
 # pygame setup
 pygame.init()
@@ -11,11 +13,16 @@ dt = 0
 
 #VHS stats
 vhsSpeed = 0
+vhsTapePos = 0
+vhsWheelRot = 0
 
-object.Object(pygame.Rect(40, 380, 40, 40), vhsSpeed)
-object.Object(pygame.Rect(40, 380, 40, 40), vhsSpeed)
-object.Object(pygame.Rect(0, 360, 40, 40), vhsSpeed)
+object.Object(pygame.Rect(90, 370, 70, 10), vhsSpeed)
+object.Object(pygame.Rect(40, 350, 30, 30), vhsSpeed)
+object.Object(pygame.Rect(0, 330, 40, 40), vhsSpeed)
 player1 = player.Player(pygame.Rect(screen.get_width()/2, screen.get_height()/2, 8, 8))
+
+tapeTexture = pygame.image.load(os.path.join("textures", "vhs tape.png"))
+wheelTexture = pygame.image.load(os.path.join("textures", "vhs wheel.png"))
 
 #Functions
 def changeVHSspeed(speed):
@@ -23,6 +30,13 @@ def changeVHSspeed(speed):
     vhsSpeed = speed
     for o in object.objects:
         o.changeVel(speed)
+
+def blitRotateCenter(surf, image, topleft, angle): #Rotates an image around its centre and draws it
+
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
+
+    surf.blit(rotated_image, new_rect)
 
 #Gameloop
 while running:
@@ -45,6 +59,7 @@ while running:
     if keys[pygame.K_k]:
         changeVHSspeed(100)
 
+    #Physics/movement and such
     for o in object.objects:
         o.rect.x += vhsSpeed * dt
 
@@ -53,14 +68,32 @@ while running:
         if (p.isGrounded):
             p.rect.x += vhsSpeed * dt
 
+    vhsTapePos += vhsSpeed * dt
+    vhsWheelRot -= vhsSpeed * dt
+
     # Render game here
     screen.fill("purple")
+
+    pygame.draw.rect(screen, "gray", pygame.Rect(25, 380, 600, 100)) #Case
+
+    screen.blit(tapeTexture, pygame.Vector2(vhsTapePos % tapeTexture.get_width(), 395))
+    screen.blit(tapeTexture, pygame.Vector2(vhsTapePos % tapeTexture.get_width() + tapeTexture.get_width(), 395))
+    screen.blit(tapeTexture, pygame.Vector2(vhsTapePos % tapeTexture.get_width() - tapeTexture.get_width(), 395))
+
+    pygame.draw.rect(screen, "gray", pygame.Rect(25, 380, 75, 100)) #Case edge
+    pygame.draw.rect(screen, "gray", pygame.Rect(550, 380, 75, 100)) #Case edge
+
+    pygame.draw.rect(screen, "purple", pygame.Rect(0, 380, 25, 100)) #BG
+    pygame.draw.rect(screen, "purple", pygame.Rect(615, 380, 25, 100)) #BG
 
     for o in object.objects:
         pygame.draw.rect(screen, "green", o.rect)
 
     for p in player.players:
         pygame.draw.rect(screen, p.color, p.rect)
+
+    blitRotateCenter(screen, wheelTexture, pygame.Vector2(450, 390), vhsWheelRot)
+    blitRotateCenter(screen, wheelTexture, pygame.Vector2(50, 390), vhsWheelRot)
 
     pygame.display.flip()
 
--- player.py
+++ player.py
@@ -17,16 +17,19 @@ class Player:
         self.gravity = 4 #The gravity strength.
         self.jumpStrength = 150 #Jump height and speed
 
+        self.isDead = False
+
         self.color = "red"
 
         players.append(self)
     
     def move(self, xdir):
-        #Check horizontal collision
-        self.velocity.x += xdir * self.speed
+        if (self.isDead == False):
+            #Check horizontal collision
+            self.velocity.x += xdir * self.speed
 
-        if (self.velocity.x.__abs__() > self.speedLimit):
-            self.velocity.x = self.velocity.x / self.velocity.x.__abs__() * self.speedLimit
+            if (self.velocity.x.__abs__() > self.speedLimit):
+                self.velocity.x = self.velocity.x / self.velocity.x.__abs__() * self.speedLimit
 
     def physicsUpdate(self, dt):
         #Horizontal movement
@@ -38,35 +41,41 @@ class Player:
 
         self.isGrounded = False
 
-        #Horizontal collision
-        for r in object.objects:
-            if (pygame.Rect.colliderect(r.rect, self.rect)):
-                self.velocity.x = r.velocity
-                if (r.rect.x > self.rect.x):
-                    self.rect.x = r.rect.x - self.rect.width
-                else:
-                    self.rect.x = r.rect.x + r.rect.width
+        if (self.isDead == False):
+            #Horizontal collision
+            for r in object.objects:
+                if (pygame.Rect.colliderect(r.rect, self.rect)):
+                    self.velocity.x = r.velocity
+                    if (r.rect.x > self.rect.x):
+                        self.rect.x = r.rect.x - self.rect.width
+                    else:
+                        self.rect.x = r.rect.x + r.rect.width
 
         #Gravity
         self.velocity.y += self.gravity
         self.rect.y += self.velocity.y * dt
 
-        #Vertical collision
-        for r in object.objects:
-            if (pygame.Rect.colliderect(r.rect, self.rect.inflate(0, 1))): 
-                if (self.velocity.y >= 0):   #Block below
-                    self.isGrounded = True
-                    self.velocity.y = 0
-                    self.rect.y = r.rect.y - self.rect.height
-                else:                       #Block above
-                    self.velocity.y = 0
-                    self.rect.y = r.rect.y + r.rect.height
+        if (self.isDead == False):
+            #Vertical collision
+            for r in object.objects:
+                if (pygame.Rect.colliderect(r.rect, self.rect.inflate(0, 1))): 
+                    if (self.velocity.y >= 0):   #Block below
+                        self.isGrounded = True
+                        self.velocity.y = 0
+                        self.rect.y = r.rect.y - self.rect.height
+                    else:                       #Block above
+                        self.velocity.y = 0
+                        self.rect.y = r.rect.y + r.rect.height
 
         #"Is grounded" logic
-        if (self.rect.y + self.rect.height >= 400 and self.isGrounded == False):
+        if (self.rect.y + self.rect.height >= 400 and self.isGrounded == False and self.rect.x <= 550 and self.rect.x + self.rect.width >= 110):
             self.isGrounded = True
             self.velocity.y = 0
             self.rect.y = 400 - self.rect.height
+
+        #Dead logic
+        if (self.rect.y > 400):
+            self.isDead = True
 
     def jump(self):
         if (self.isGrounded):
